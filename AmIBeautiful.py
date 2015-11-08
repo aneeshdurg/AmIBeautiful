9ab901f5f1f4446ccbefb297c988f2d85a4530dd
aneeshdurg <durg2@illinois.edu>
2015-11-08 00:15:23 -0600

    Some changes and updates

--- AmIBeautiful.py
+++ AmIBeautiful.py
@@ -2,37 +2,6 @@ import cv2
 import sys
 #from VideoCapture import Device
 
-cam = cv2.VideoCapture(0)
-s, img = cam.read()
-if s:    # frame captured without any errors
-    cv2.namedWindow("cam-test")
-    cv2.imshow("cam-test",img)
-    cv2.waitKey(0)
-    cv2.destroyWindow("cam-test")
-    cv2.imwrite("webcam.jpg",img)
-# Get user supplied values
-imagePath = 'webcam.jpg'
-
-cascPathf = "haarcascade_frontalface_default.xml"
-cascPathe = "parojos.xml"
-cascPathm = "Mouth.xml"
-cascPathn = "Nariz_nuevo_20stages.xml"
-
-# Create the haar cascade
-faceCascade = cv2.CascadeClassifier(cascPathf)
-eyeCascade = cv2.CascadeClassifier(cascPathe)
-mouthCascade = cv2.CascadeClassifier(cascPathm)
-noseCascade = cv2.CascadeClassifier(cascPathn)
-
-# Read the image
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-# Detect faces in the image
-eyes = eyeCascade.detectMultiScale(gray)
-faces = faceCascade.detectMultiScale(gray)
-lips = mouthCascade.detectMultiScale(gray)
-noses = noseCascade.detectMultiScale(gray)
 
 # print "Found {0} Faces!".format(len(faces))
 # print "Found {0} Eyes!".format(len(eyes))
@@ -47,35 +16,75 @@ proportions=[]
 realnose=[]
 realmouth=[]
 ratios=[]
-for (x, y, w, h) in faces:
-	cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
-	for(xe, ye, we, he) in eyes:
-		if xe in xrange(x, x+w) and ye in xrange(y, y+h):
-			cv2.rectangle(image, (xe, ye), (xe+we, ye+he), (0, 255, 0), 2)
-			print ("Eye seperation is: "+str(we)+"px")
-			for(xm, ym, wm, hm) in lips:
-				if xm and xm+wm in xrange(xe, xe+we) and ym and ym+hm in xrange(ye+he, y+h) and ym>ye+he:
-					realmouth.append([xm, ym, wm, hm])
-			maxmouth=[0, 0, 0, 0]
-			for mouth in realmouth:
-				if mouth[1]>maxmouth[1]:
-					maxmouth=mouth
-			if not maxmouth==[0, 0, 0, 0]:
-				xm, ym, wm, hm=maxmouth[0], maxmouth[1], maxmouth[2], maxmouth[3]				
-				cv2.rectangle(image, (xm, ym), (xm+wm, ym+hm), (0, 0, 225), 2)	
-				print ("Mouth width is: "+str(wm)+"px")
-				for(xn, yn, wn, hn) in noses:
-					if xn and xn+wn in xrange(xe, xe+we) and yn and yn+hn in xrange(ye+he, ym):
-						realnose.append([xn, yn, wn, hn])
-				maxnose=[0, 0, 0, 0]
-				for nose in realnose:
-					if nose[0]>maxnose[0]:
-						maxnose=nose
-				if not maxnose==[0, 0, 0, 0]:
-					xn, yn, wn, hn=maxnose[0], maxnose[1], maxnose[2], maxnose[3]
-					cv2.rectangle(image, (xn, yn), (xn+wn, yn+hn), (0, 225, 225), 2)							
-					print ("Nose is at: ("+str((xn+wn)/2)+", "+str((yn+hn)/2)+")")
-					proportions.append([[x, y, w, h],[xe, ye, we, he], [xm, ym, wm, hm], [xn, yn, wn, hn]])	
+while True:
+	print "Press enter key to take a picture!"
+	hacks=raw_input()
+	cam = cv2.VideoCapture(0)
+	s, img = cam.read()
+	if s:    # frame captured without any errors
+	    cv2.namedWindow("cam-test")
+	    cv2.imshow("cam-test",img)
+	    cv2.waitKey(0)
+	    cv2.destroyWindow("cam-test")
+	    cv2.imwrite("webcam.jpg",img)
+	# Get user supplied values
+	imagePath = 'webcam.jpg'
+	if not hacks=="":
+		imagePath = hacks+'.jpg'
+
+	cascPathf = "haarcascade_frontalface_default.xml"
+	cascPathe = "parojos.xml"
+	cascPathm = "Mouth.xml"
+	cascPathn = "Nariz_nuevo_20stages.xml"
+
+	# Create the haar cascade
+	faceCascade = cv2.CascadeClassifier(cascPathf)
+	eyeCascade = cv2.CascadeClassifier(cascPathe)
+	mouthCascade = cv2.CascadeClassifier(cascPathm)
+	noseCascade = cv2.CascadeClassifier(cascPathn)
+
+	# Read the image
+	image = cv2.imread(imagePath)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+	# Detect faces in the image
+	eyes = eyeCascade.detectMultiScale(gray)
+	faces = faceCascade.detectMultiScale(gray)
+	lips = mouthCascade.detectMultiScale(gray)
+	noses = noseCascade.detectMultiScale(gray)
+	for (x, y, w, h) in faces:
+		cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+		for(xe, ye, we, he) in eyes:
+			if xe in xrange(x, x+w) and ye in xrange(y, y+h):
+				cv2.rectangle(image, (xe, ye), (xe+we, ye+he), (0, 255, 0), 2)
+				print ("Eye seperation is: "+str(we)+"px")
+				for(xm, ym, wm, hm) in lips:
+					if xm and xm+wm in xrange(xe, xe+we) and ym and ym+hm in xrange(ye+he, y+h) and ym>ye+he:
+						realmouth.append([xm, ym, wm, hm])
+				maxmouth=[0, 0, 0, 0]
+				for mouth in realmouth:
+					if mouth[1]>maxmouth[1]:
+						maxmouth=mouth
+				if not maxmouth==[0, 0, 0, 0]:
+					xm, ym, wm, hm=maxmouth[0], maxmouth[1], maxmouth[2], maxmouth[3]				
+					cv2.rectangle(image, (xm, ym), (xm+wm, ym+hm), (0, 0, 225), 2)	
+					print ("Mouth width is: "+str(wm)+"px")
+					for(xn, yn, wn, hn) in noses:
+						if xn and xn+wn in xrange(xe, xe+we) and yn and yn+hn in xrange(ye+he, ym):
+							realnose.append([xn, yn, wn, hn])
+							if len(realnose)==0:
+								print "Detection failed! Trying again..."
+								continue
+					maxnose=[0, 0, 0, 0]
+					for nose in realnose:
+						if nose[0]>maxnose[0]:
+							maxnose=nose
+					if not maxnose==[0, 0, 0, 0]:
+						xn, yn, wn, hn=maxnose[0], maxnose[1], maxnose[2], maxnose[3]
+						cv2.rectangle(image, (xn, yn), (xn+wn, yn+hn), (0, 225, 225), 2)							
+						print ("Nose is at: ("+str((xn+wn)/2)+", "+str((yn+hn)/2)+")")
+						proportions.append([[x, y, w, h],[xe, ye, we, he], [xm, ym, wm, hm], [xn, yn, wn, hn]])	
+	break					
     
 cv2.imshow("Faces found", image)
 cv2.waitKey(0)
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # AmIBeautiful
-
 Analyses a face or faces (from webcam) and gives you a rating of how beautiful you are!
 
 The score is between 0 and 100.
@@ -8,5 +7,10 @@ You will need to install numpy.
 
 Run with the command:
 >python AmIBeautiful.py
+
+All cascade sheets found at:
+>http://alereimondo.no-ip.org/OpenCV/34
+
+![AmIBeautiful](http://i.imgur.com/0ITvAuk.png "AmIBeautiful")
 
 >Made at YHack 2015
